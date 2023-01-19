712c491d0d144320f0c22d487ebbc4edefdbe29e
katkakor <kordiovskak@gmail.com>
2023-01-19 17:22:18 +0100

    updated

--- projekt_1.py
+++ projekt_1.py
@@ -10,7 +10,7 @@ discord: Katerina K#9622
 user = input("Name:")
 password = input("Password:")
 line = "-" * 40
-registrovani_uzivatele = {
+registered_users = {
     "bob": "123",
     "ann": "pass123",
     "mike": "password123",
@@ -45,7 +45,7 @@ garpike and stingray are also present.'''
 ]
 
 #zjistí, jestli zadané údaje odpovídají někomu z registrovaných uživatelů
-if registrovani_uzivatele.get(user) == password:
+if registered_users.get(user) == password:
 #pokud je registrovaný, pozdrav jej a umožni mu analyzovat texty,
     print(f"Username", {user})
     print(f"Password", {password})
@@ -71,20 +71,20 @@ else:
 
 #počet slov
 analyzed_text = (TEXTS[int(selected_text) - 1])
-delka = len(analyzed_text.split())
-print(f"There are", {delka}, "words in the selected text.")
+lenght = len(analyzed_text.split())
+print(f"There are", {lenght}, "words in the selected text.")
 
 #sumu všech čísel (ne cifer) v textu.
-mezery = analyzed_text.split()
+spaces = analyzed_text.split()
 count = 0
-for c in mezery:
+for c in spaces:
     if c.isnumeric():
         count = count + int(c)
 print(f"The sum of all the numbers", {count})
               
 #počet čísel (ne cifer)   
 cnt = 0
-for a in mezery:
+for a in spaces:
     if a.isnumeric():
         cnt = cnt + 1
 print(f"There are", {cnt}, "numeric strings.")
@@ -94,7 +94,7 @@ print(f"There are", {cnt}, "numeric strings.")
 newstring = ''
 count1 = 0
 count2 = 0
-for a in mezery:
+for a in spaces:
     if (a.isupper()) == True and (a.isalpha()):
         count1 += 1
         newstring += (a.lower())
@@ -105,35 +105,35 @@ print("There are", {count1}, "uppercase words.")
 print("There are", {count2}, "lowercase words.")
 
 #počet slov začínajících velkým písmenem
-pocet = 0
+count3 = 0
 mezery = analyzed_text.split()
 for index, symbol in enumerate(mezery):
     if symbol[0].isupper():
-        pocet = pocet + 1
-print(f"There are", {pocet}, "titlecase words.")
+        count3 = count3 + 1
+print(f"There are", {count3}, "titlecase words.")
 
 
 #Program zobrazí jednoduchý sloupcový graf, který bude reprezentovat četnost různých délek slov v textu
 # ocisteni slov od carek a tecek 
-ocistena_slova = list()
-for a in mezery:
-    ocistena_slova.append(
+words = list()
+for a in spaces:
+    words.append(
         a.strip(".,").lower()
     )
 # pridani slov a cetnost slov se stejnym poctem do slovniku 
-novy_slovnik = dict()
-for x in ocistena_slova:
-    if len(x) not in novy_slovnik:
-        novy_slovnik[len(x)] = 1        
+new_dict = dict()
+for x in words:
+    if len(x) not in new_dict:
+        new_dict[len(x)] = 1        
     else:
-        novy_slovnik[len(x)] = novy_slovnik[len(x)] + 1
+        new_dict[len(x)] = new_dict[len(x)] + 1
 
 # serazeni
-serazeno = sorted(novy_slovnik.items())
+dict_sorted = sorted(new_dict.items())
 print(line)
 print("LEN|".ljust(2), "\tOCCURENCES","|NR.".rjust(5))
 print(line)
 # vypsani cetnosti slov
-for index, symbol in serazeno:
+for index, symbol in dict_sorted:
      print(str(index).rjust(2), "|", symbol*"*", (12-symbol+1)*" ", "|", str(symbol))
 
